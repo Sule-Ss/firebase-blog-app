15905797ed2adb3f6e0a35f76c5d3061f8e8a12d
Sule <aybilenden@gmail.com>
2022-04-23 01:38:13 +0300

    part-6

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Details = () => {
+  //router da url e eklenen id yi almak için :
+  // const {id} = useParams();
   return (
     <div>Details</div>
   )
